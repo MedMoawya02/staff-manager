59515d0f684ecfff43724301ff0907e13942ee66
MedMoawya02 <mohammed.mouawya2002@gmail.com>
2025-11-17 17:24:39 +0100

    feat:Sauvegarde le tableau des employés dans LocalStorage pour maintenir les données après rafraîchissement.

--- script.js
+++ script.js
@@ -7,8 +7,20 @@ let nom=document.getElementById('workerName');
 let role=document.getElementById('workerRole');
 let image=document.getElementById('inputImageWorker');
 let btnClose=document.querySelector('.close');
+let imageWorker=document.getElementById('imageWorker');
+
+let company=document.getElementById('workerExperienceCompany');
+let workerExperienceRole=document.getElementById('workerExperienceRole');
+let dateDebutExperience=document.getElementById('dateDebutExperience');
+let dateFinExperience=document.getElementById('dateFinExperience');
 //
-let workers=[];
+let workers;
+if(localStorage.getItem('workers')){
+    workers=JSON.parse(localStorage.getItem('workers'))
+}else{
+    workers=[];
+}
+showWorkers();
 //Afficher/cacher le formulaire 
 btnAddWorker.addEventListener('click',()=>{
     document.getElementById('formulaire').style.display="flex";
@@ -17,13 +29,14 @@ btnClose.addEventListener('click',()=>{
     document.getElementById('formulaire').style.display="none";
 })
 
+
 //show data
 function showWorkers(){
     staffCards.innerHTML=workers.map((worker,index)=>
         `
             <div class="cardStaff">
                     <div class="cardInfo">
-                        <img src= class="imgCard">
+                        <img src='${worker.image}' class="imgCard">
                         <div>
                             <h3>${worker.nom}</h3>
                             <p>${worker.role}</p>
@@ -31,28 +44,42 @@ function showWorkers(){
                     </div>
                     <button class="btnEditStaff">Edit</button>
                 </div> 
-        `
-       
+        `   
     ).join("")
 }
 
 //formulaire submit
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
+    let newExperience={
+        company:company.value,
+        workerExperienceRole:workerExperienceRole.value,
+        dateDebutExperience:dateDebutExperience.value,
+        dateFinExperience:dateFinExperience.value,
+    }
     let newWorker={
         nom:nom.value,
         role:role.value,
-        image:image.value,
+        image:imageWorker.src,
+        experience:newExperience,
     }
     if(nom.value==""||role.value==""){
         alert("Veuillez saisez une value")
     }else{
-        workers.push(newWorker)
-
-    }
-    console.log(newWorker);
-    console.log(workers);
-    showWorkers();
-    form.reset();
-    
+        workers.push(newWorker);
+        localStorage.setItem('workers',JSON.stringify(workers))    
+        showWorkers();
+        form.reset();
+        imageWorker.src="";
+        imageWorker.style.display="none";
+        document.getElementById('formulaire').style.display="none";
+        console.log(workers);
+        
+    }  
 })
+
+//L'affichage de l'image
+function changeImage(){
+    imageWorker.src=URL.createObjectURL(image.files[0]);
+    imageWorker.style.display="block";
+}
