6c65afa6c361996546acd023ddf24e9723973fc8
MedMoawya02 <mohammed.mouawya2002@gmail.com>
2025-11-19 15:27:38 +0100

    feat:l'ajout de la fonction assignToRoom pour diviser les employées à partir ses roles

--- script.js
+++ script.js
@@ -13,21 +13,26 @@ let company = document.getElementById('workerExperienceCompany');
 let workerExperienceRole = document.getElementById('workerExperienceRole');
 let dateDebutExperience = document.getElementById('dateDebutExperience');
 let dateFinExperience = document.getElementById('dateFinExperience');
-let btnAnnuler= document.getElementById('btnAnnuler');
+let btnAnnuler = document.getElementById('btnAnnuler');
 
 let errorParagraphe = document.querySelectorAll('#formAddWorker .errorMessage');
 
 //data for rooms section
-let btnsAddToRoom=document.querySelectorAll('#rooms .room-item .addToRoom');
+let receptionSalle =[];
+let salleServeur=[];
+let salleSécurité=[];
+let manager=[];
+let btnsAddToRoom = document.querySelectorAll('#rooms .room-item .addToRoom');
+let receptionRoom = document.querySelector('.room-3');
 
 //data for modal section
-let modalSection=document.getElementById('modal');
-let modalContent=document.getElementById('modalContent');
+let modalSection = document.getElementById('modal');
+let modalContent = document.getElementById('modalContent');
 
 
 //local storage
 let workers;
-if (localStorage.getItem('workers')!=null) {
+if (localStorage.getItem('workers') != null) {
     workers = JSON.parse(localStorage.getItem('workers'))
 } else {
     workers = [];
@@ -69,7 +74,7 @@ function showWorkers() {
 
 //formulaire submit
 form.addEventListener('submit', (e) => {
-    e.preventDefault();       
+    e.preventDefault();
     let newExperience = {
         company: company.value,
         workerExperienceRole: workerExperienceRole.value,
@@ -90,14 +95,14 @@ form.addEventListener('submit', (e) => {
         errorParagraphe[0].innerHTML = "Le nom doit contenir au moins 2 lettres."
         errorParagraphe[0].style.display = "block";
         isValid = false;
-    }else{
+    } else {
         errorParagraphe[0].style.display = "none";
     }
     if (role.value == "" || role.value == "Option") {
         errorParagraphe[1].innerHTML = "Veuillez saisir un role principal."
         errorParagraphe[1].style.display = "block";
         isValid = false;
-    }else{
+    } else {
         errorParagraphe[1].style.display = "none";
     }
     /*  if (imageWorker.src && imageWorker.src !== "" && !urlRegex.test(imageWorker.src)) {
@@ -108,7 +113,7 @@ form.addEventListener('submit', (e) => {
         errorParagraphe[2].innerHTML = "La date de début de l'expérience ne peut pas être après la date de fin."
         errorParagraphe[2].style.display = "block";
         isValid = false;
-    }else{
+    } else {
         errorParagraphe[2].style.display = "none";
     }
 
@@ -120,7 +125,7 @@ form.addEventListener('submit', (e) => {
         showWorkers();
         form.reset();
         imageWorker.src = "";
-   /*      imageWorker.style.display = "none"; */
+        /*      imageWorker.style.display = "none"; */
         document.getElementById('formulaire').style.display = "none";
         console.log(workers);
         console.log(errorParagraphe);
@@ -143,47 +148,112 @@ function changeImage(src) {
 
 
 //L'etape de diviser les employées
-btnsAddToRoom.forEach(btn=>{
-    btn.addEventListener('click',(e)=>{
-        /* console.log(btn.dataset.action); */
+let assignRoom=null;
+btnsAddToRoom.forEach(btn => {
+    btn.addEventListener('click', (e) => {
         modalSection.style.display = "flex";
-        modalContent.innerHTML=`<h3>Select worker to assign</h3>${
-
-            workers.map((worker, index) =>
+        modalContent.innerHTML = `<h3>Select worker to assign</h3>${workers.map((worker, index) =>
             `
-                <div class="cardStaff" data-id="${index}">
-                        <div class="cardInfo">
-                            <img src='${worker.image}' class="imgCard">
-                            <div>
-                                <h3>${worker.nom}</h3>
-                                <p>${worker.role}</p>
-                            </div>
-                        </div>
-                        <button class="btnEditStaff">Edit</button>
-                </div> 
-    
+            <div class="cardStaff modalCards" data-id="${index}">
+            <div class="cardInfo">
+            <img src='${worker.image}' class="imgCard">
+            <div>
+            <h3>${worker.nom}</h3>
+            <p>${worker.role}</p>
+            </div>
+            </div>
+            <button class="btnEditStaff">Edit</button>
+            </div> 
             `
         ).join("")
-        }
-        <button id="btnCloseModal">Close</button>
-        `
-        document.getElementById("btnCloseModal").addEventListener('click',()=>{
-                modalSection.style.display = "none";
+            }
+    <button id="btnCloseModal">Close</button>
+    `
+        console.log(btn.dataset.action);     
+        assignRoom=btn.dataset.action;
+        document.getElementById("btnCloseModal").addEventListener('click', () => {
+            modalSection.style.display = "none";
         })
     });
 });
 
+modalSection.addEventListener('click', (e) => {   
+    let clickedCard = e.target.closest('.modalCards');
+    if (clickedCard) {
+        let idAssigned = clickedCard.dataset.id;
+        let workerRole=workers[idAssigned].role;
+        
+        console.log(clickedCard.closest('room-item'));
+        
+        //salle de reception
+        if(assignRoom=="assignToRéceptionRoom"){
+             if (workerRole == "Réceptionniste"||workerRole == "Manager") {
+                let [workerAssigne] = workers.splice(idAssigned, 1);
+                receptionSalle.push(workerAssigne);
+                assignToRoom(receptionSalle, 'room-3');
+            }  
+            else {
+                alert("you should assign just receptionniste or manager" );
+            }
 
-//une fonction pour le button annuler qui cache le formulaire
-/* btnAnnuler.addEventListener('click',(e)=>{
-    e.preventDefault();
-    document.getElementById('formulaire').style.display = "none";
+            console.log(manager);
+        }
+
+        //salle des serveurs
+        if(assignRoom=="assignToServerRoom"){
+            if(workerRole=="Technicien IT"||workerRole == "Manager"){
+                let [workerAssigned]=workers.splice(idAssigned,1);
+                salleServeur.push(workerAssigned);
+                assignToRoom(salleServeur,"room-4");
+            }else{
+                alert("you should assign just technicien it or manager");
+            }
+        }
+
+        //salle de securité
+        if(assignRoom=="assignToSecurityRoom"){
+            if(workerRole=="Agent de sécurité"||workerRole == "Manager"){
+                let [workerAssigned]=workers.splice(idAssigned,1);
+                salleSécurité.push(workerAssigned);
+                assignToRoom(salleSécurité,"room-5")
+                
+            }else{
+                alert("you should assign just agent sécurité or manager");
+            }
+        }
+           
+        modalSection.style.display="none";
+        localStorage.setItem('workers',JSON.stringify(workers));
+        showWorkers();
+    }
 })
 
+//fonction pour ajouter chaque employer à son salle 
+function assignToRoom(workers,roomClass){
+    document.querySelector('.'+roomClass+' div').innerHTML=`${workers.map((worker, index) =>
+            `
+            <div class="cardStaff modalCards" data-id="${index}">
+            <div class="cardInfo">
+            <img src='${worker.image}' class="imgCard">
+            <div>
+            <h3>${worker.nom}</h3>
+            <p>${worker.role}</p>
+            </div>
+            </div>
+            <button class="deleteWorker" type="button"data-room-index="${index}" data-room-array-name="${roomClass}">&#x2715</button>
+            </div> 
+            `
+        ).join("")
+            }
 
-document.querySelectorAll('.cardStaff').forEach(card=>{
-    card.addEventListener('click',(e)=>{
-        console.log(card.dataset.id);
+    `
+}
+//fonction pour retirer un employé depuis une salle 
+document.getElementById('rooms').addEventListener('click',(e)=>{
+    let btnDelete=e.target.closest('.deleteWorker');
+    if(btnDelete){
+        console.log(btnDelete);
         
-    })
-} )*/
+    }
+
+})
